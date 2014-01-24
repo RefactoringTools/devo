9d4e3b854b32511ce41f677cdd08bc5eb619b560
huiqing <hl@kent.ac.uk>
2014-01-24 14:22:02 +0000

    added one more step.

--- tests/test_s_group.erl
+++ tests/test_s_group.erl
@@ -20,7 +20,12 @@ run() ->
     io:format("Add node3 to group1...\n"),
     timer:sleep(1000),
     Res3=s_group:add_nodes(group1, ['node3@127.0.0.1']),
-    io:format("Add nodes to s_group result:~p\n", [Res3]),
+    io:format("Add nodes to group1 result:~p\n", [Res3]),
+    io:get_chars("Press any key to continue ...\n", 1),
+    io:format("Remove node3 from group1...\n"),
+    timer:sleep(1000),
+    Res4=s_group:remove_nodes(group1, ['node3@127.0.0.1']),
+    io:format("remove nodes from group1 result:~p\n", [Res4]),
     ok.
 
     
