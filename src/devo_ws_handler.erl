53bdb4040e2ddaf92934e336beb243aee6270aae
Stephen A <StephenAdams.Minn@gmail.com>
2014-06-19 23:16:40 +0100

    Moved profiling functions to their own module. Started refactoring.

--- src/devo_ws_handler.erl
+++ src/devo_ws_handler.erl
@@ -30,7 +30,7 @@ websocket_init(_TransportName, Req, _Opts) ->
 websocket_handle({text, <<"stop">>}, Req, State) ->
     Cmd = State#state.cmd, 
     Nodes = State#state.nodes,
-    stop_profiling(Cmd, Nodes),
+    profiling:stop_profiling(Cmd, Nodes),
     {shutdown, Req, State};
 websocket_handle({text, <<"start">>}, Req, State) ->
     {ok, Req, State};
@@ -45,14 +45,14 @@ websocket_handle({text, Msg}, Req, State) ->
             Ns = [list_to_atom(N)||N<-Nodes],
             Cmd=message_queue_len,
             %% Hardcoded process name, will be removed.
-            start_profiling(Cmd, {sd_orbit, Ns}),
+            profiling:start_profiling(Cmd, {sd_orbit, Ns}, self()),
             NewState=State#state{cmd=Cmd, nodes=Ns},
             {ok, Req, NewState};
         ["start_profile",Feature, NodeStr] ->
             Nodes= string:tokens(NodeStr, ";"),
             Ns = [list_to_atom(N)||N<-Nodes],
             Cmd = list_to_atom(Feature),
-            start_profiling(Cmd, Ns),
+            profiling:start_profiling(Cmd, Ns, self()),
             NewState=State#state{cmd=Cmd, nodes=Ns},
             {ok, Req, NewState};
         _ ->
@@ -139,182 +139,6 @@ websocket_info(Info, Req, State) ->
 websocket_terminate(_Reason, _Req, _State) ->
 	ok.
 
-start_profiling(rq, [Node|_]) ->
-    case rpc:call(Node, erlang, system_info, [cpu_topology]) of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to start profiling for reason:\n~p\n",
-                           [Reason]);
-        Cpu ->
-            case Cpu of 
-                undefined ->
-                    self()!{cpu, cpu(erlang:system_info(schedulers))};
-                Cpu ->
-                    self()!{cpu, Cpu}
-            end,
-            Res=rpc:call(Node, devo_sampling, start,
-                         [[run_queues], infinity, none, {devo, node()}]),
-            case Res of 
-                {badrpc, Reason} ->
-                    io:format("Devo failed to start profiling for reason:\n~p\n",
-                              [Reason]);
-                _ -> Res
-            end
-    end;
-start_profiling(migration, [Node|_]) ->
-    case rpc:call(Node, erlang, system_info, [cpu_topology]) of
-        {badrpc, Reason} ->
-            io:format("Devo failed to start profiling for reason:\n~p\n",
-                      [Reason]);
-        Cpu ->
-            case Cpu of 
-                undefined ->
-                    self()!{cpu, cpu(erlang:system_info(schedulers))};
-                Cpu ->
-                    self()!{cpu, Cpu}
-            end,
-            erlang:start_timer(1, self(), <<"Online profiling started...!">>),
-            devo_trace:start_trace(migration, Node, {devo, node()})
-    end;
-start_profiling(rq_migration, [Node|_]) ->
-    case rpc:call(Node, erlang, system_info, [cpu_topology]) of
-        {badrpc, Reason} ->
-            io:format("Devo failed to start profiling for reason:\n~p\n",
-                      [Reason]);
-        Cpu ->
-            case Cpu of 
-                undefined ->
-                    self()!{cpu, cpu(erlang:system_info(schedulers))};
-                Cpu ->
-                    self()!{cpu, Cpu}
-            end,
-            erlang:start_timer(1, self(), <<"Online profiling started...!">>),
-            Res=rpc:call(Node, devo_sampling, start,
-                         [[run_queues], infinity, none,{devo, node()}]),
-            case Res of 
-                {badrpc, Reason} ->
-                    io:format("Devo failed to start profiling for reason:\n~p\n",
-                              [Reason]);
-                _ -> 
-                    devo_trace:start_trace(migration, Node, {devo, node()})
-            end
-    end;
-start_profiling(message_queue_len, {RegName, [Node|_]}) ->
-  %%  erlang:start_timer(1, self(), <<"Online profiling started...!">>),
-    Res=rpc:call(Node, devo_sampling, start,
-                 [[{message_queue_len, RegName}], infinity, none,{devo, node()}]),
-    case Res of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to start profiling for reason:\n~p\n",
-                      [Reason]);
-        _ -> Res
-    end;
-start_profiling(inter_node, Nodes=[N|_Ns]) ->
-    case get_init_s_group_config(N) of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to start profiling for reason:\n~p\n",
-                      [Reason]);
-        undefined ->
-            self()!{s_group_init_config, []},
-            erlang:start_timer(1, self(), <<"Online profiling started...!">>),
-            devo_trace:start_trace(inter_node, Nodes, {devo, node()});
-        {ok, NodeGrps} ->
-            self()!{s_group_init_config, NodeGrps},
-            erlang:start_timer(1, self(), <<"Online profiling started...!">>),
-            devo_trace:start_trace(inter_node, Nodes, {devo, node()})            
-    end;
-start_profiling(s_group, Nodes=[N|_Ns]) ->
-    case get_init_s_group_config(N) of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to start profiling for reason:\n~p\n",
-                      [Reason]);
-        undefined ->
-            self()!{s_group_init_config, []},
-            devo_trace:start_trace(s_group, Nodes, {devo, node()});
-        {ok, NodeGrps} ->
-            self()!{s_group_init_config, NodeGrps},
-            devo_trace:start_trace(s_group, Nodes, {devo, node()})
-    end;
-start_profiling(Cmd, _Nodes) ->
-    io:format("start_profiling: unnexpected command:~p\n", [Cmd]),
-    ok.
-
-
-stop_profiling(rq, [Node|_]) ->
-    Res=rpc:call(Node, devo_sampling, stop,[]),
-    case Res of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to stop profiling for reason:\n~p\n",
-                      [Reason]);
-        _ -> 
-           Res
-    end;
-stop_profiling(message_queue_len, [Node|_]) ->
-    Res=rpc:call(Node, devo_sampling, stop,[]),
-    case Res of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to stop profiling for reason:\n~p\n",
-                      [Reason]);
-        _ -> 
-           Res
-    end;
-
-stop_profiling(migration, [_Node|_]) ->
-    devo_trace:stop_trace();
-stop_profiling(rq_migration, [Node|_]) ->
-    Res=rpc:call(Node, devo_sampling, stop,[]),
-    case Res of 
-        {badrpc, Reason} ->
-            io:format("Devo failed to stop profiling for reason:\n~p\n",
-                      [Reason]);
-        _ -> 
-            Res
-    end,
-    erlang:start_timer(1, self(), stop_profile),
-    devo_trace:stop_trace();
-stop_profiling(inter_node, _Nodes) ->
-    erlang:start_timer(1, self(), stop_profile),
-    devo_trace:stop_trace();
-stop_profiling(s_group, _Nodes) ->
-    devo_trace:stop_trace();
-stop_profiling(undefined,_) ->
-    ok;
-stop_profiling(Cmd, _Nodes) ->
-    io:format("stop_profiling: unnexpected command:~p\n", [Cmd]),
-    ok.
-
-get_init_s_group_config(Node) ->
-    case rpc:call(Node, application, get_env, [kernel, s_groups]) of
-        {badrpc, Reason} ->
-            {badrpc, Reason};
-        undefined ->
-            {ok, []};
-        {ok, NodeGrps} ->
-            io:fwrite("~w~n", [NodeGrps]),
-            Grps = [grp_tuple(NodeGrp)||NodeGrp<-NodeGrps],
-            {ok,Grps}
-    end.
-
-grp_tuple({Name, Nodes}) ->
-    {Name, Nodes};  
-grp_tuple({Name, _, Nodes}) ->
-    {Name, Nodes}.
-
 rm_whites(Str) ->
     [C||C<-Str, C=/=$\s, C=/=$\r,  C=/=$\n].
-    
-
-%% fake cpu topology.
-cpu(8)->
-    {cpu,[{processor,[{core,[{thread,{logical,0}},{thread,{logical,1}}]},
-                      {core,[{thread,{logical,2}},{thread,{logical,3}}]},
-                      {core,[{thread,{logical,4}},{thread,{logical,5}}]},
-                      {core,[{thread,{logical,6}},{thread,{logical,7}}]}]}]};
-cpu(4) ->
-    {cpu,[{processor,[{core,[{thread,{logical,0}},{thread,{logical,1}}]},
-                      {core,[{thread,{logical,2}},{thread,{logical,3}}]}]}]};
-cpu(2) ->
-    {cpu,[{processor,[{core,[{thread,{logical,0}},{thread,{logical,1}}]}]}]};
-cpu(_N) ->
-    io:format("Warning: cpu_topology undefined!\n"),
-    undefined.
     
